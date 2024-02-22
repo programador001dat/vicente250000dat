7419f8ff069c78291625ad9e2a9fa74dab90ba94
vicente250000dat <155966937+vicente250000dat@users.noreply.github.com>
2024-02-22 12:51:59 -0300

    Add files via upload

---
+++ Tabuada2x9.py
@@ -0,0 +1,189 @@
+print ('\n                  °TABUADA DO [2 ao 9]')
+print('-------------------------------------------------------------\n')
+while True:
+    for valor2 in range(1):
+        for valor3 in range(1):
+            for valor4 in range(1):
+                for valor5 in range(1):
+                    valor2 = (2 * 1)
+                    valor3 = (3 * 1)
+                    valor4 = (4 * 1)
+                    valor5 = (5 * 1)
+                    print(' [2x1:]',valor2, '       [3x1:]',valor3, '       [4x1:]',valor4,'       [5x1:]',valor5)
+    for valor2 in range(1):
+        for valor3 in range(1):
+            for valor4 in range(1):
+                for valor5 in range(1):
+                    valor2 = (2 * 2)
+                    valor3 = (3 * 2)
+                    valor4 = (4 * 2)
+                    valor5 = (5 * 2)
+                    print(' [2x2:]',valor2, '       [3x2:]',valor3,  '       [4x2:]',valor4,'       [5x2:]',valor5)
+    for valor2 in range(1):
+        for valor3 in range(1):
+            for valor4 in range(1):
+                for valor5 in range(1):
+                    valor2 = (2 * 3)
+                    valor3 = (3 * 3)
+                    valor4 = (4 * 3)
+                    valor5 = (5 * 3)
+                    print(' [2x3:]',valor2, '       [3x3:]',valor3, '       [4x3:]',valor4,'      [5x3:]',valor5)
+    for valor2 in range(1):
+        for valor3 in range(1):
+            for valor4 in range(1):
+                for valor5 in range(1):
+                    valor2 = (2 * 4)
+                    valor3 = (3 * 4)
+                    valor4 = (4 * 4)
+                    valor5 = (5 * 4)
+                    print(' [2x4:]',valor2,'       [3x4:]',valor3, '      [4x4:]',valor4,'      [5x4:]',valor5)
+    for valor2 in range(1):
+        for valor3 in range(1):
+            for valor4 in range(1):
+                for valor5 in range(1):
+                    valor2 = (2 * 5)
+                    valor3 = (3 * 5)
+                    valor4 = (4 * 5)
+                    valor5 = (5 * 5)
+                    print(' [2x5:]',valor2, '      [3x5:]',valor3, '      [4x5:]',valor4,'      [5x5:]',valor5)
+    for valor2 in range(1):
+        for valor3 in range(1):
+            for valor4 in range(1):
+                for valor5 in range(1):
+                    valor2 = (2 * 6)
+                    valor3 = (3 * 6)
+                    valor4 = (4 * 6)
+                    valor5 = (5 * 6)
+                    print(' [2x6:]',valor2,'      [3x6:]',valor3,  '      [4x6:]',valor4,'      [5x6:]',valor5)
+    for valor2 in range(1):
+        for valor3 in range(1):
+            for valor4 in range(1):
+                for valor5 in range(1):
+                    valor2 = (2 * 7)
+                    valor3 = (3 * 7)
+                    valor4 = (4 * 7)
+                    valor5 = (5 * 7)
+                    print(' [2x7:]',valor2,'      [3x7:]',valor3, '      [4x7:]',valor4,'      [5x7:]',valor5)
+    for valor2 in range(1):
+        for valor3 in range(1):
+            for valor4 in range(1):
+                for valor5 in range(1):
+                    valor2 = (2 * 8)
+                    valor3 = (3 * 8)
+                    valor4 = (4 * 8)
+                    valor5 = (5 * 8)
+                    print(' [2x8:]',valor2,'      [3x8:]',valor3, '      [4x8:]',valor4,'      [5x8:]',valor5)
+    for valor2 in range(1):
+        for valor3 in range(1):
+            for valor4 in range(1):
+                for valor5 in range(1):
+                    valor2 = (2 * 9)
+                    valor3 = (3 * 9)
+                    valor4 = (4 * 9)
+                    valor5 = (5 * 9)
+                    print(' [2x9:]',valor2,'      [3x9:]',valor3, '      [4x9:]',valor4,'      [5x9:]',valor5)
+    for valor2 in range(1):
+        for valor10 in range(1):
+            for valor4 in range(1):
+                for valor5 in range(1):
+                    valor2 = (2 * 10)
+                    valor3 = (3 * 10)
+                    valor4 = (4 * 10)
+                    valor5 = (5 * 10)
+                    print(' [2x10:]',valor2,'     [3x10:]',valor3,  '     [4x10:]',valor4,'    '
+                                                                                          ' [5x10:]',valor5)
+                    print('\n')
+    #break
+    for valor6 in range(1):
+        for valor7 in range(1):
+            for valor8 in range(1):
+                for valor9 in range(1):
+                    valor6 = (6 * 1)
+                    valor7 = (7 * 1)
+                    valor8 = (8 * 1)
+                    valor9 = (9 * 1)
+                    print(' [6x1:]',valor6,'       [7x1:]',valor7,'       [8x1:]',valor8,'        [9x1:]',valor9)
+    for valor6 in range(1):
+        for valor7 in range(1):
+            for valor8 in range(1):
+                for valor10 in range(1):
+                    valor6 = (6 * 2)
+                    valor7 = (7 * 2)
+                    valor8 = (8 * 2)
+                    valor9 = (9 * 2)
+                    print(' [6x2:]',valor6,'      [7x2:]',valor7,'      [8x2:]',valor8,'       [9x2:]',valor9)
+    for valor6 in range(1):
+        for valor7 in range(1):
+            for valor8 in range(1):
+                for valor9 in range(1):
+                    valor6 = (6 * 3)
+                    valor7 = (7 * 3)
+                    valor8 = (8 * 3)
+                    valor9 = (9 * 3)
+                    print(' [6x3:]', valor6, '      [7x3:]', valor7,'      [8x3:]',valor8,'       [9x3:]',valor9)
+    for valor6 in range(1):
+        for valor7 in range(1):
+            for valor8 in range(1):
+                for valor9 in range(1):
+                    valor6 = (6 * 4)
+                    valor7 = (7 * 4)
+                    valor8 = (8 * 4)
+                    valor9 = (9 * 4)
+                    print(' [6x4:]',valor6,'      [7x4:]',valor7,'      [8x4:]',valor8,'       [9x4:]',valor9)
+    for valor6 in range(1):
+        for valor7 in range(1):
+            for valor8 in range(1):
+                for valor9 in range(1):
+                    valor6 = (6 * 5)
+                    valor7 = (7 * 5)
+                    valor8 = (8 * 5)
+                    valor9 = (9 * 5)
+                    print(' [6x5:]',valor6,'      [7x5:]',valor7,'      [8x5:]',valor8,'       [9x5:]',valor9)
+    for valor6 in range(1):
+        for valor7 in range(1):
+            for valor8 in range(1):
+                for valor9 in range(1):
+                    for valor9 in range(1):
+                        valor6 = (6 * 6)
+                        valor7 = (7 * 6)
+                        valor8 = (8 * 6)
+                        valor9 = (9 * 6)
+                        print(' [6x6:]',valor6,'      [7x6:]',valor7,'      [8x6:]',valor8,'       [9x6:]',valor9)
+    for valor6 in range(1):
+        for valor7 in range(1):
+            for valor8 in range(1):
+                for valor9 in range(1):
+                    valor6 = (6 * 7)
+                    valor7 = (7 * 7)
+                    valor8 = (8 * 7)
+                    valor9 = (9 * 7)
+                    print(' [6x7:]',valor6,'      [7x7:]',valor7,'      [8x7:]',valor8,'       [9x7:]',valor9)
+    for valor6 in range(1):
+        for valor7 in range(1):
+            for valor8 in range(1):
+                for valor9 in range(1):
+                    valor6 = (6 * 8)
+                    valor7 = (7 * 8)
+                    valor8 = (8 * 8)
+                    valor9 = (9 * 8)
+                    print(' [6x8:]',valor6,'      [7x8:]',valor7,'      [8x8:]',valor8,'       [9x8:]',valor9)
+    for valor6 in range(1):
+        for valor7 in range(1):
+            for valor8 in range(1):
+                for valor9 in range(1):
+                    valor6 = (6 * 9)
+                    valor7 = (7 * 9)
+                    valor8 = (8 * 9)
+                    valor9 = (9 * 9)
+                    print(' [6x9:]',valor6,'      [7x9:]',valor7,'      [8x9:]',valor8,'       [9x9:]',valor9)
+    for valor6 in range(1):
+        for valor7 in range(1):
+            for valor8 in range(1):
+                for valor9 in range(1):
+                    valor6 = (6 * 10)
+                    valor7 = (7 * 10)
+                    valor8 = (8 * 10)
+                    valor9 = (9 * 10)
+                    print(' [6x10:]',valor6,'     [7x10:]',valor7,'     [8x10:]',valor8,'      [9x10:]',valor9)
+    print('\n--------------------------------------------------------------')
+    break
